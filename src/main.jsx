56eab310e78c3e58d7202a488e10008cdb398e1a
Sais-opu <saidulislamapu2024@gmail.com>
2025-08-04 10:53:53 +0600

    path working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router =createBrowserRouter([
+  {
+    path
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
